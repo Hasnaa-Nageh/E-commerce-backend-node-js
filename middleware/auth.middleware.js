d87d7e1fe13d32935eb1ddc5ad6c6f521f65fe52
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-08-29 10:00:03 +0300

    update

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,22 +1,16 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
 function authenticateToken(req, res, next) {
-  // Look for token in Authorization header
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // format: "Bearer <token>"
-
+  const token = req.cookies.accessToken;
   if (!token) {
     return res.status(401).json({ message: "Not authenticated" });
   }
-
   try {
     const payload = jwt.verify(token, process.env.ACCESS_TOKEN);
-    req.user = payload; // attach user data to request
+    req.user = payload;
     next();
   } catch (err) {
     return res.status(403).json({ message: "Invalid or expired token" });
   }
 }
-
 module.exports = { authenticateToken };
