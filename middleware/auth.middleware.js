40ca6b945ad684074f2a263a5c32f5c8b2c56a61
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-08-29 10:22:29 +0300

    update

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
 
   if (!token) return res.status(401).json({ message: "Not authenticated" });
 
